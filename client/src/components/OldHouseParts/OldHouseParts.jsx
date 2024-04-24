2257c83f701c0cb5672443d84d7e20b0de1a9d15
Jake-loranger <jloranger0@gmail.com>
2024-04-23 20:15:43 -0400

    OHP Backend and SEO

--- client/src/components/OldHouseParts/OldHouseParts.jsx
+++ client/src/components/OldHouseParts/OldHouseParts.jsx
@@ -4,7 +4,8 @@ import './oldhouseparts.css';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import structure from '../../assets/img/project-img/OldHouseParts-img/structure.png';
-import demo from '../../assets/img/project-img/OldHouseParts-img/demoOHP.mp4'
+import demoOHP from '../../assets/img/project-img/OldHouseParts-img/demoOHP.mp4';
+import demoBackend from '../../assets/img/project-img/OldHouseParts-img/demoBackend.mp4';
 
 const OldHouseParts = ({ show, handleClose }) => {
     const [activeTab, setActiveTab] = useState('overview');
@@ -46,20 +47,24 @@ const Overview = () => (
             <Col className="d-flex align-items-start">
                 <div className='mb-md-5 px-lg-5'>
                     <h4 className="mb-3 text-decoration-underline">Overview</h4>
-                    <p className='pe-lg-5 pe-md-3'>The Old House Parts Co. website is a full-stack application developed for a local antique shop using the
-                        React framework. This custom-designed platform features a user-friendly frontend with five distinct pages: Home, Inventory, About,
-                        FAQs, and Contact. The backend of the application is supported by a robust MySQL database, which stores all the inventory data. It
-                        facilitates various operations such as adding, editing, and archiving inventory items through a PHP-based API that handles both
-                        POST and GET requests. The frontend, designed to retrieve inventory information, primarily makes GET requests. The entire application
-                        was designed and developed in-house, incorporating technologies such as React.js, Bootstrap, HTML, CSS, and JavaScript to create a
-                        seamless and responsive user experience.</p>
+                    <p className='pe-lg-5 pe-md-3'>The Old House Parts Co. website is a full-stack
+                        application developed for a local antique shop using the React framework.
+                        This custom-designed platform features a user-friendly frontend with five distinct pages:
+                        Home, Inventory, About, FAQs, and Contact. The backend of the application is supported by a
+                        robust MySQL database, which stores all the inventory data. It facilitates various operations
+                        such as adding, editing, and archiving inventory items through a PHP-based API that handles both POST and
+                        GET requests. The frontend, designed to retrieve inventory information, primarily makes GET requests.
+                        The entire application was designed and developed in-house, incorporating technologies such as
+                        React.js, Bootstrap, HTML, CSS, and JavaScript to create a seamless and responsive user experience.</p>
+
+                    <p>Please reach out if you would like to see the full source code, as this is contained in a private repo for security reasons.</p>
 
                     <a href='https://rockycoastlabs.xyz/'><h5><br></br>Check out the website here</h5></a>
                 </div>
             </Col>
             <Col xs={12} md={12} lg={5} className="d-flex align-items-center">
                 <video className='mx-auto' width="100%" height="auto" autoPlay loop muted>
-                    <source src={demo} type="video/mp4" />
+                    <source src={demoOHP} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </Col>
@@ -229,15 +234,280 @@ const Frontend = () => {
 };
 
 const Backend = () => {
+
+    const phpPOST =
+        `
+        $con = mysqli_connect($host, $user, $password, $dbname);
+
+        $name = $_POST['name'];
+        $dimensionW = $_POST['dimensionW'];
+        $dimensionH = $_POST['dimensionH'];
+        $dimensionD = $_POST['dimensionD'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+        $description = $_POST['description'];
+        $category = $_POST['category'];
+        $subCategory = $_POST['subCategory'];
+        $doorPanels = $_POST['doorPanels'];
+        $shipping = $_POST['shipping'];
+
+        $uniqueIdentifier = uniqid();
+        $uploadPath = 'uploads/' . $uniqueIdentifier . '/'; // Your upload folder with a unique identifier
+        $imagePaths = array();
+        
+        if (!is_dir($uploadPath)) {
+            mkdir($uploadPath, 0755, true);
+        }
+        
+        for ($i = 1; $i <= 3; $i++) {
+            $fileKey = 'image' . $i;
+            $targetFile = $uploadPath . basename($_FILES[$fileKey]['name']);
+        
+            if (!empty($_FILES[$fileKey]['tmp_name']) && file_exists($_FILES[$fileKey]['tmp_name'])) {
+                if (move_uploaded_file($_FILES[$fileKey]['tmp_name'], $targetFile)) {
+                    $imagePaths[$i - 1] = $targetFile;
+                } else {
+                    echo "Error uploading file " . $fileKey;
+                }
+            } else {
+                $imagePaths[$i - 1] = NULL;
+            }
+        }
+        
+        $imagePath1 = $imagePaths[0];
+        $imagePath2 = $imagePaths[1];
+        $imagePath3 = $imagePaths[2];
+    
+        $sql = "INSERT INTO Inventory (name, dimensionH, dimensionW, dimensionD, price, quantity, description, category, sub_category,  door_panels, shipping, image_path1, image_path2, image_path3)
+        VALUES ('$name', '$dimensionH', '$dimensionW', '$dimensionD', '$price', '$quantity', '$description', '$category', '$subCategory', '$doorPanels', '$shipping', '$imagePath1', '$imagePath2', '$imagePath3')";
+        
+        if (mysqli_query($con, $sql)) {
+            echo "Data inserted successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
+        `
+
+    const dataStruc =
+        `const initialState = {
+            image1: null,
+            image2: null,
+            image3: null,
+            date: '',
+            name: '',
+            dimensionH: '',
+            dimensionW: '',
+            dimensionD: '',
+            price: '',
+            quantity: '',
+            description: '',
+            category: '',
+            subCategory: '',
+            doorPanels: '',
+            shipping: '0',
+        };
+        
+        const [formData, setFormData] = useState(initialState);`
+
+    const formControl =
+        `<Form.Control 
+        type="name" 
+        name="name"
+        onChange={handleInputChange} required 
+        />`
+
+    const handleInput =
+        `const handleInputChange = (e) => {
+            const { name, value } = e.target;
+    
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        };`
+
+
     return (
         <>
+            <Row xs={1} md={1} lg={2} className="g-5 mx-auto mt-md-5 mt-sm-3">
+                <Col className="d-flex align-items-start">
+                    <div className='mb-md-5 px-lg-5'>
+                        <h4 className="mb-3 text-decoration-underline">Demo</h4>
+                        <p className='pe-lg-5 pe-md-3'>This video demonstrates the process of adding and editing an inventory item. Once the "Post to Website" buttton is pressed,
+                            the data contained within the form is saved to a MySQL database with a POST method API call using PHP. The frontend website pulls data from this database based on
+                            the parameters such as category, subcategory, recency, inventory ID, etc. When inventory information is edited and confirmed, a PHP file makes a POST call to
+                            the database and changes the parameters that are different than the current ones. </p>
+                    </div>
+                </Col>
+                <Col xs={12} md={12} lg={5} className="d-flex align-items-center">
+                    <video className='mx-auto' width="100%" height="auto" autoPlay loop muted>
+                        <source src={demoBackend} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </Col>
+            </Row>
+
+            <Row xs={1} md={1} lg={2} className="g-5 mx-auto my-md-5 my-sm-3">
+                <Col xs={12} md={12} lg={6} className="d-flex align-items-start">
+                    <div className='mb-md-5 px-lg-5'>
+                        <h4 className="mb-3 text-decoration-underline">Form Overview</h4>
+                        <p className='pe-lg-5 pe-md-3'>
+                            These three code snippets are integral components of the AddForm.jsx component.
+                            The first snippet establishes the initial state structure for form data, and facilitates
+                            the state management via the useState hook. The second snippet defines the form control
+                            for the name input field within the Form component, which controls the event handling.
+                            The third snippet defines the handleInputChange function, triggered upon any user input
+                            change, dynamically updating the corresponding field within the form data state, thereby
+                            ensuring real-time synchronization between user input and component state.
+                            Together, these snippets contribute to a robust form handling mechanism within the AddForm component.
+                        </p>
+                    </div>
+                </Col>
+                <Col xs={12} md={12} lg={6}>
+                    <SyntaxHighlighter language="javascript" style={solarizedlight}>
+                        {dataStruc}
+                    </SyntaxHighlighter>
+                    <SyntaxHighlighter language="javascript" style={solarizedlight}>
+                        {formControl}
+                    </SyntaxHighlighter>
+                    <SyntaxHighlighter language="javascript" style={solarizedlight}>
+                        {handleInput}
+                    </SyntaxHighlighter>
+                </Col>
+            </Row>
+
+            <Row xs={1} md={1} lg={2} className="g-5 mx-auto my-md-5 my-sm-3">
+                <Col xs={12} md={12} lg={6} className="d-flex align-items-start">
+                    <div className='mb-md-5 px-lg-5'>
+                        <h4 className="mb-3 text-decoration-underline">PHP POST Function</h4>
+                        <p className='pe-lg-5 pe-md-3'>
+                            When a POST request is initiated to upload a new inventory item, the
+                            upload PHP file is invoked. This file orchestrates the collection of
+                            inventory data from the backend and subsequently executes a query in the
+                            SQL database to integrate the new item. It also manages the storage
+                            of inventory images by organizing them into item-specific folders. The file
+                            then records the URL paths of these images within the corresponding image
+                            parameters in the SQL database.
+                        </p>
+                    </div>
+                </Col>
+                <Col xs={12} md={12} lg={6}>
+                    <SyntaxHighlighter language="php" style={solarizedlight}>
+                        {phpPOST}
+                    </SyntaxHighlighter>
+                </Col>
+            </Row>
+
+
         </>
     )
 };
 
 const SEO = () => {
+    const sitemap =
+        `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://www.oldhouseparts.com</loc>
+            <lastmod>2024-02-22</lastmod>
+            <changefreq>yearly</changefreq>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>https://www.oldhouseparts.com/inventory</loc>
+            <lastmod>2024-02-21</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        <url>
+            <loc>https://www.oldhouseparts.com/about</loc>
+            <lastmod>2024-02-21</lastmod>
+            <changefreq>yearly</changefreq>
+            <priority>0.6</priority>
+        </url>
+        <url>
+            <loc>https://www.oldhouseparts.com/faqs</loc>
+            <lastmod>2024-02-21</lastmod>
+            <changefreq>yearly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        <url>
+            <loc>https://www.oldhouseparts.com/contact</loc>
+            <lastmod>2024-02-21</lastmod>
+            <changefreq>yearly</changefreq>
+            <priority>0.6</priority>
+        </url>
+    </urlset>`
+
+    const HomeHelmet =
+        `<Helmet>
+         <title>Old House Parts - Home</title>
+         <meta name="description" 
+         content="Embark on a journey to Old House Parts & Company, 
+         in the charming town of Kennebunk, Maine just a two hour 
+         drive from Boston. Our antique sanctuary resides within an 
+         11,000 square foot 1872 freight warehouse, a testament to 
+         the rich history that awaits inside. " />
+         
+         <meta name="keywords" 
+         content="Old House Parts, 
+         Antique Shop, Kennebunk, Maine, Architectural salvage, Vintage Furniture, Collectibles" />
+     
+         <meta property="og:url" content="https://www.oldhousepartsco.com" />
+         <meta property="og:title" content="Old House Parts Co." />
+         <meta property="og:description" content="Explore our collection of vintage furniture, art, and collectibles at Old House Parts Co. in Kennebunk, Maine." />
+         <meta property="og:image" content={metaImage} />
+     
+         <meta property="article:publisher" content={socialLinks.facebook} />
+         <meta property="instagram:site" content={socialLinks.instagram} />
+         <meta property="youtube:channel" content={socialLinks.youtube} />
+</Helmet>`
+
     return (
         <>
+            <Row xs={1} md={1} lg={2} className="g-5 mx-auto my-md-5 my-sm-3">
+                <Col xs={12} md={12} lg={6} className="d-flex align-items-start">
+                    <div className='mb-md-5 px-lg-5'>
+                        <h4 className="mb-3 text-decoration-underline">Sitemap</h4>
+                        <p className='pe-lg-5 pe-md-3'>
+                            A sitemap is a file that lists all the important pages on a website
+                            to help search engines understand its structure. This sitemap for
+                            rockycoastlabs.xyz includes URLs for the homepage, inventory, about,
+                            FAQs, and contact pages, along with details like the last modification
+                            date, change frequency, and priority level for each page. It provides
+                            search engines with valuable information to efficiently crawl and index
+                            the website's content.
+                        </p>
+                    </div>
+                </Col>
+                <Col xs={12} md={12} lg={6}>
+                    <SyntaxHighlighter language="xml" style={solarizedlight}>
+                        {sitemap}
+                    </SyntaxHighlighter>
+                </Col>
+            </Row>
+
+            <Row xs={1} md={1} lg={2} className="g-5 mx-auto my-md-5 my-sm-3">
+                <Col xs={12} md={12} lg={6} className="d-flex align-items-start">
+                    <div className='mb-md-5 px-lg-5'>
+                        <h4 className="mb-3 text-decoration-underline">Sitemap</h4>
+                        <p className='pe-lg-5 pe-md-3'>
+                        The Helmet component 
+                        allows for the inclusion of metadata such as the page title, description, 
+                        and keywords, which significantly contribute to search engine visibility 
+                        and user engagement. The Helmet enables the integration of social 
+                        media metadata, facilitating seamless sharing and enhancing the website's presence 
+                        across various platforms. Overall, it helps improve both the functionality and visibility of web pages within search engines.
+                        </p>
+                    </div>
+                </Col>
+                <Col xs={12} md={12} lg={6}>
+                    <SyntaxHighlighter language="xml" style={solarizedlight}>
+                        {HomeHelmet}
+                    </SyntaxHighlighter>
+                </Col>
+            </Row>
+
         </>
     )
 };
